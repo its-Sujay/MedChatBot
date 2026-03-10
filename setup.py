b353075fe5328f37bfcf4f653799c9b962f0131c
its-Sujay <sujayragothaman@gmail.com>
2026-03-10 21:53:14 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name = "MedChatBot",
+    version = "0.1.0",
+    author = "Sujay R",
+    author_email = "sujayragothaman@gmail.com",
+    packages = find_packages(),
+    install_requires = []   
+)
